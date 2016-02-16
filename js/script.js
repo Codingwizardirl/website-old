818a689161a1951e5f0e469a280686c617110b79
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2016-02-16 17:26:03 +0000

    added image carousel and new photos

--- js/script.js
+++ js/script.js
@@ -21,3 +21,21 @@
         $('.navbar').fadeIn();
     }
    },false);
+
+    $("#nav ul li a[href^='#']").on('click', function(e) {
+
+       // prevent default anchor click behavior
+       e.preventDefault();
+
+       // animate
+       $('html, body').animate({
+           scrollTop: $(this.hash).offset().top
+         }, 300, function(){
+   
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = this.hash;
+         });
+
+    });
+

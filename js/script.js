664bcf44eb4ef426cd16bd708f4fffd8dc183ef7
Pavel Georgiev <pavel.r.georgiev@hotmail.com>
2016-04-14 21:48:23 +0100

    added JS for the navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+ 
+$(document).ready(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+
+    window.addEventListener("scroll", function() {
+    if (window.scrollY > 150) {
+        $('.navbar').fadeOut();
+    }
+    else {
+        $('.navbar').fadeIn();
+    }
+   },false);
+
+    $("#nav ul li a[href^='#']").on('click', function(e) {
+
+       // prevent default anchor click behavior
+       e.preventDefault();
+
+       // animate
+       $('html, body').animate({
+           scrollTop: $(this.hash).offset().top
+         }, 300, function(){
+   
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = this.hash;
+         });
+
+    });
+
 $(document).ready(function() {
     $('#contact_form').bootstrapValidator({
         // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
@@ -70,52 +112,8 @@ $(document).ready(function() {
                 console.log(result);
             }, 'json');
         });
+
+
 });
 
-$(document).ready(function() {
-         $('.carousel').carousel({
-             interval: 8000
-         })
-    });    
-
- $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                scrollTop: target.offset().top
-              }, 1000);
-              return false;
-            }
-          }
-        });
-      });
-
-    window.addEventListener("scroll", function() {
-    if (window.scrollY > 150) {
-        $('.navbar').fadeOut();
-    }
-    else {
-        $('.navbar').fadeIn();
-    }
-   },false);
-
-    $("#nav ul li a[href^='#']").on('click', function(e) {
-
-       // prevent default anchor click behavior
-       e.preventDefault();
-
-       // animate
-       $('html, body').animate({
-           scrollTop: $(this.hash).offset().top
-         }, 300, function(){
-   
-           // when done, add hash to url
-           // (default click behaviour)
-           window.location.hash = this.hash;
-         });
-
-    });
 
